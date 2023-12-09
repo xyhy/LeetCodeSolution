82cc73c3c013a029d9bff0e469f85477ff2266af
xyhy <32924559+xyhy@users.noreply.github.com>
2023-12-09 17:01:22 +0800

    Time: 103 ms (28.45%), Space: 94.3 MB (7.09%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -5,8 +5,8 @@ public:
         int maxi=0;
         stored[prices.size()-1]=0;
         for (int i=prices.size()-2; i>=0; i--){
-            stored[i]= std::max(stored[i+1]+prices[i+1]-prices[i],prices[i+1]-prices[i]);
-            maxi=std::max(maxi, stored[i]);
+            stored[i]= (stored[i+1]+prices[i+1]-prices[i]>prices[i+1]-prices[i])?stored[i+1]+prices[i+1]-prices[i]:prices[i+1]-prices[i];
+            maxi=(maxi>stored[i])?maxi:stored[i];
         }
         return maxi;
     }
